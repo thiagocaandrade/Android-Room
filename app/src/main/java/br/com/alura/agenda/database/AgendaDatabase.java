50e43e9f36139a42491f9d7eb110669f212ceedf
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-31 09:00:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -0,0 +1,31 @@
+package br.com.alura.agenda.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+import br.com.alura.agenda.database.converter.ConversorCalendar;
+import br.com.alura.agenda.database.dao.RoomAlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+import static br.com.alura.agenda.database.AgendaMigrations.TODAS_MIGRATIONS;
+
+@Database(entities = {Aluno.class}, version = 4, exportSchema = false)
+@TypeConverters({ConversorCalendar.class})
+public abstract class AgendaDatabase extends RoomDatabase {
+
+    private static final String NOME_BANCO_DE_DADOS = "agenda.db";
+
+
+    public abstract RoomAlunoDAO getRoomAlunoDAO();
+
+    public static AgendaDatabase getInstance(Context context) {
+        return Room.databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
+                .allowMainThreadQueries()
+                .addMigrations(TODAS_MIGRATIONS)
+                .build();
+    }
+
+}
---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,70 @@
+package br.com.alura.agenda.model;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+@Entity
+public class Aluno implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id = 0;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    @Ignore
+    private Calendar momentoDeCadastro = Calendar.getInstance();
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return nome + " - " + telefone;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean temIdValido() {
+        return id > 0;
+    }
+
+    public String dataFormatada(){
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        return formatador.format(momentoDeCadastro.getTime());
+    }
+}
